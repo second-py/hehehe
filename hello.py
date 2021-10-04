54dbc62159a3115750c53ce9fccc0ecd726a25c8
second-py <pooya98@nate.com>
2021-10-04 22:24:50 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+// For command line git tutorial
+// First : GitHub modification
+// Second : Local repository modification
+
+print("Hello World")
+print("Tell Your World")
+print("Tell His World")
+print("Tell Her World")
+print("Tell My World")
+print("Tell A")
+print("Tell B")
+print("Tell D")
